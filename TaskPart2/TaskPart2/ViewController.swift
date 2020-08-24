66279049bb916d56e49941e87cae0a6b8145a351
miya1186 <66915986+miya1186@users.noreply.github.com>
2020-08-24 16:45:15 +0900

    first commit

---
+++ TaskPart2/TaskPart2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskPart2
+//
+//  Created by miyazawaryohei on 2020/08/23.
+//  Copyright © 2020 miya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
